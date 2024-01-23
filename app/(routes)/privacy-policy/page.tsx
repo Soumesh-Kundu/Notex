bf07774e92bedba2c8e75b0a4972e4b6024e9711
Soumesh Kundu <iamsoumo26@gmail.com>
2024-01-23 22:28:30 +0530

    v1: privacy policy page added

---
+++ app/(routes)/privacy-policy/page.tsx
@@ -0,0 +1,148 @@
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <main className="max-w-screen-lg mx-auto px-2 lg:px-0 py-10">
+        <h1 className="text-4xl md:text-[42px] font-bold ">Privacy Policy</h1>
+
+        <p className="py-3">
+          <strong>Effective Date: 23-01-2024</strong>
+        </p>
+        <h3 className="text-2xl md:text-3xl font-bold mb-3">Your privacy is important to us</h3>
+
+        <p className=" mb-2">
+          It is Notex's policy to respect your privacy regarding any information
+          we may collect while operating our website. This Privacy Policy
+          applies to{" "}
+          <a href="https://notex-next.vercel.app"> notex-next.vercel.app/</a>{" "}
+          (hereinafter, "us", "we", or "notex-next.vercel.app"). We respect
+          your privacy and are committed to protecting personally identifiable
+          information you may provide us through the Website. We have adopted
+          this privacy policy ("Privacy Policy") to explain what information may
+          be collected on our Website, how we use this information, and under
+          what circumstances we may disclose the information to third parties.
+          This Privacy Policy applies only to information we collect through the
+          Website and does not apply to our collection of information from other
+          sources.
+        </p>
+        <p className="">
+          This Privacy Policy, together with the Terms of service posted on our
+          Website, set forth the general rules and policies governing your use
+          of our Website. Depending on your activities when visiting our
+          Website, you may be required to agree to additional terms of service.
+        </p>
+        <h2 id="tableofcontents" className="text-2xl md:text-3xl font-bold my-3 ">Contents</h2>
+        <p>Click below to jump to any section of this privacy policy</p>
+        <ol type="1" className="list-decimal list-inside mx-10 my-4">
+          <li>
+            <a href="#PII">
+              <strong>Personally-Identifying Information</strong>
+            </a>
+          </li>
+          <li>
+            <a href="#Security">
+              <strong>Security</strong>
+            </a>
+          </li>
+          <li>
+            <a href="#Cookies">
+              <strong>Cookies</strong>
+            </a>
+          </li>
+          <li>
+            <a href="#Changes">
+              <strong>Privacy Policy Changes</strong>
+            </a>
+          </li>
+          <li>
+            <a href="#Credit">
+              <strong>Contact Information &amp; Credit</strong>
+            </a>
+          </li>
+        </ol>
+        <h2 id="PII" className="text-2xl md:text-3xl font-bold my-3">1. Personally-Identifying Information</h2>
+        <p className="mb-2">
+          Certain visitors to Notex's websites choose to interact with Notex in
+          ways that require Notex to gather personally-identifying information.
+          The amount and type of information that Notex gathers depends on the
+          nature of the interaction. For example, we ask visitors who leave a
+          comment at https://notex-next.vercel.app to provide a username and
+          email address.
+        </p>
+
+        <p>
+          <a href="#tableofcontents" className="font-semibold">Back to table of contents</a>
+        </p>
+        <h2 id="Security" className="text-2xl md:text-3xl font-bold my-3">2. Security</h2>
+        <p className="mb-2">
+          The security of your Personal Information is important to us, but
+          remember that no method of transmission over the Internet, or method
+          of electronic storage is 100% secure. While we strive to use
+          commercially acceptable means to protect your Personal Information, we
+          cannot guarantee its absolute security.
+        </p>
+
+        <p>
+          <a href="#tableofcontents" className="font-semibold">Back to table of contents</a>
+        </p>
+        <h2 id="Cookies" className="text-2xl md:text-3xl font-bold my-3">3. Cookies</h2>
+        <p className="mb-2">
+          To enrich and perfect your online experience, Notex uses "Cookies",
+          similar technologies and services provided by others to display
+          personalized content, appropriate advertising and store your
+          preferences on your computer.
+        </p>
+        <p className="mb-2">
+          A cookie is a string of information that a website stores on a
+          visitor's computer, and that the visitor's browser provides to the
+          website each time the visitor returns. Notex uses cookies to help
+          Notex identify and track visitors, their usage of
+          https://notex-next.vercel.app, and their website access preferences.
+          Notex visitors who do not wish to have cookies placed on their
+          computers should set their browsers to refuse cookies before using
+          Notex's websites, with the drawback that certain features of Notex's
+          websites may not function properly without the aid of cookies.
+        </p>
+        <p className="mb-2">
+          By continuing to navigate our website without changing your cookie
+          settings, you hereby acknowledge and agree to Notex's use of cookies.
+        </p>
+
+        <p>
+          <a href="#tableofcontents" className="font-semibold">Back to table of contents</a>
+        </p>
+        <h2 id="Changes" className="text-2xl md:text-3xl font-bold my-3">4. Privacy Policy Changes</h2>
+        <p className="mb-2">
+          Although most changes are likely to be minor, Notex may change its
+          Privacy Policy from time to time, and in Notex's sole discretion.
+          Notex encourages visitors to frequently check this page for any
+          changes to its Privacy Policy. Your continued use of this site after
+          any change in this Privacy Policy will constitute your acceptance of
+          such change.
+        </p>
+
+        <p>
+          <a href="#tableofcontents" className="font-semibold">Back to table of contents</a>
+        </p>
+        <h2 id="Credit" className="text-2xl md:text-3xl font-bold my-3">5. Contact Information &amp; Credit</h2>
+        <p className="mb-2">
+          This privacy policy was created at{" "}
+          <a
+            style={{ color: "inherit", textDecoration: "none" }}
+            href="https://privacyterms.io/privacy-policy-generator/"
+            title="Privacy policy generator"
+            target="_blank"
+          >
+            privacyterms.io privacy policy generator
+          </a>
+          . If you have any questions about our Privacy Policy, please contact
+          us via <a href="mailto:iamsoumo2004@gmail.com">email</a> or{" "}
+          <a href="tel:">phone</a>.
+        </p>
+
+        <p>
+          <a href="#tableofcontents" className="font-semibold mt-2">Back to table of contents</a>
+        </p>
+      </main>
+    </>
+  );
+}
